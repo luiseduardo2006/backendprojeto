3ffb65a0a67577dda34f23719c593fb03164acd9
luiseduardo2006 <luiseeduardosilva00@gmail.com>
2024-11-23 09:43:21 -0300

    Delete project directory

--- project/consulta_produtos.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const darkModeEnabled = localStorage.getItem('darkModeEnabled') === 'true';
-    if (darkModeEnabled) document.body.classList.add('dark-mode');
-});
-
-function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
-    localStorage.setItem('darkModeEnabled', document.body.classList.contains('dark-mode'));
-}
